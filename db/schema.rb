dca5a51dbf2c4fc6e1ba596da7f2204eb0b267d3
Veenie <raveen08@gmail.com>
2021-08-19 15:15:36 -0700

    updates to users for admin and banker, amount to transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_202211) do
+ActiveRecord::Schema.define(version: 2021_08_19_220815) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_202211) do
     t.integer "account_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "amount", precision: 8, scale: 2
     t.index ["account_id"], name: "index_transactions_on_account_id"
   end
 
@@ -37,6 +38,8 @@ ActiveRecord::Schema.define(version: 2021_08_19_202211) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.boolean "banker", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
