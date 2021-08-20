0edbe5232a6b01559f9e6d5e39d65b86e72233e0
Veenie <raveen08@gmail.com>
2021-08-19 19:47:42 -0700

    rollback balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,14 @@ class Account < ApplicationRecord
   belongs_to :user
   has_many :transactions
 
+
+  after_initialize :default_values
+ 
+  def default_values
+
+    self.balance ||= 500.00
+  end
+
   def balance
     self.transactions.sum(:amount)
   end
